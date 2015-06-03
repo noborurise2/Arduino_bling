925fd42d03473c751c4d1719e80c7fbed4fc586f
unknown <ac2252uyan@PC318.ed.y.kumamoto-nct.ac.jp>
2015-06-03 11:03:23 +0900

    11秒待って1秒間ずつ点滅するスケッチ

---
+++ Arduino_bling.ino
@@ -0,0 +1,15 @@
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(13, OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  delay(11000);
+  for (int i=0;i<17;i++){
+    digitalWrite(13, HIGH);
+    delay(1000);
+    digitalWrite(13, LOW);
+    delay(1000);
+  }
+}
